e1d77d8ad92a3a4d676d8206679a38852a3ec967
Enzo falcon <falcone134@gmail.com>
2025-12-16 17:52:50 -0500

    Creando archivo de constantes

---
+++ constants.py
@@ -0,0 +1,131 @@
+STRUCTURE_DATA = {
+    "perro": {
+        "categorias": [
+            # {
+            #     "Alimentos": {
+            #         "id": "CATG15475",
+            #         "category_name": "Alimento-para-perros",
+            #     }
+            # },
+            # {
+            #     "Platos": {
+            #         "id": "cat14110477",
+            #         "category_name": "Platos--dispensadores-y-botellas",
+            #     }
+            # },
+            # {
+            #     "Antiparasitarios": {
+            #         "id": "CATG15478",
+            #         "category_name": "Antiparasitarios",
+            #     }
+            # },
+            # {
+            #     "Camas": {
+            #         "id": "CATG15476",
+            #         "category_name": "Camas-y-colchones-para-perro",
+            #     }
+            # },
+            {
+                "Casas": {
+                    "id": "CATG15477",
+                    "category_name": "Casas-para-perros",
+                }
+            },
+            # {
+            #     "Jaulas y transporte": {
+            #         "id": "cat12500467",
+            #         "category_name": "Jaulas-y-transporte-para-perros",
+            #     }
+            # },
+            # {
+            #     "Arnes": {
+            #         "id": "cat14110481",
+            #         "category_name": "Arnes--Collares-y-Correas",
+            #     }
+            # },
+            # {
+            #     "Peluqueria canina": {
+            #         "id": "CATG15481",
+            #         "category_name": "Peluqueria-canina",
+            #     }
+            # },
+            # {
+            #     "Juguetes y entrenamiento": {
+            #         "id": "cat12500465",
+            #         "category_name": "Juguetes-y-entrenamiento-para-perro",
+            #     }
+            # },
+            # {
+            #     "Ropa y accesorios": {
+            #         "id": "cat12500466",
+            #         "category_name": "Ropa-y-accesorios-para-perros",
+            #     }
+            # },
+            # Es el mismo que Antiparasitarios
+            # {
+            #     "Higiene y salud canina": {
+            #         "id": "CATG15478",
+            #         "category_name": "Antiparasitarios",
+            #     }
+            # },
+        ]
+    },
+    # "gato": {
+    #     "categorias": [
+    #         {
+    #             "Alimentos": {
+    #                 "id": "CATG15470",
+    #                 "category_name": "Alimento-para-Gatos",
+    #             }
+    #         },
+    #         {
+    #             "Platos": {
+    #                 "id": "CATG33635",
+    #                 "category_name": "Platos-para-gatos",
+    #             }
+    #         },
+    #         {
+    #             "Arena": {
+    #                 "id": "CATG15472",
+    #                 "category_name": "Arena",
+    #             }
+    #         },
+    #         {
+    #             "Areneros": {
+    #                 "id": "CATG33754",
+    #                 "category_name": "Areneros",
+    #             }
+    #         },
+    #         {
+    #             "Camas": {
+    #                 "id": "CATG14641",
+    #                 "category_name": "Camas-para-gatos",
+    #             }
+    #         },
+    #         {
+    #             "Rascadores": {
+    #                 "id": "CATG33636",
+    #                 "category_name": "Rascadores-para-gato",
+    #             }
+    #         },
+    #         {
+    #             "Juguetes": {
+    #                 "id": "CATG14643",
+    #                 "category_name": "Juguetes-y-entretencion-para-gatos",
+    #             }
+    #         },
+    #         {
+    #             "Arneses y collares": {
+    #                 "id": "CATG14640",
+    #                 "category_name": "Arneses-y-collares",
+    #             }
+    #         },
+    #         {
+    #             "Higiene y cuidado": {
+    #                 "id": "CATG14642",
+    #                 "category_name": "Higiene-y-cuidados-para-gatos",
+    #             }
+    #         },
+    #     ]
+    # },
+}
