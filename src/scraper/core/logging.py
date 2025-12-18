f6303326a62b6fcb704b4ef2cd9840e42b472c56
Enzo Falcon <falcone134@gmail.com>
2025-12-18 08:49:54 -0500

    Actualizar logging para que se guarde por fecha

--- src/scraper/core/logging.py
+++ src/scraper/core/logging.py
@@ -1,5 +1,5 @@
 import logging
-from logging.handlers import RotatingFileHandler
+from logging.handlers import TimedRotatingFileHandler
 
 from scraper.core.settings import settings
 
@@ -17,17 +17,25 @@ def get_logger(name: str) -> logging.Logger:
         "%(asctime)s | %(levelname)s | %(name)s | %(message)s"
     )
 
-    # consola
+    # ---------- Consola ----------
     console = logging.StreamHandler()
+    console.setLevel(level)
     console.setFormatter(formatter)
 
-    # archivo
-    file_handler = RotatingFileHandler(
-        settings.LOG_DIR / f"{settings.APP_NAME}.log",
-        maxBytes=5_000_000,
-        backupCount=5,
+    # ---------- Archivo (rotación diaria) ----------
+    file_handler = TimedRotatingFileHandler(
+        filename=settings.LOG_DIR / "logs.log",
+        when="midnight",  # rota a medianoche
+        interval=1,  # cada 1 día
+        backupCount=7,  # conserva 7 días de logs
         encoding="utf-8",
+        utc=False,  # usa hora local (pon True si usas UTC)
     )
+
+    # sufijo del archivo rotado
+    file_handler.suffix = "%Y-%m-%d"
+
+    file_handler.setLevel(level)
     file_handler.setFormatter(formatter)
 
     logger.addHandler(console)
