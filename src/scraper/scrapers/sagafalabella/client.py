4afe028e618e2de6bd8af6672ef236305724ba92
Enzo Falcon <falcone134@gmail.com>
2025-12-23 08:30:05 -0500

    Agregando fetch de detalle de producto en html

--- src/scraper/scrapers/sagafalabella/client.py
+++ src/scraper/scrapers/sagafalabella/client.py
@@ -15,9 +15,22 @@ def fetch_products_page(page, category_id, category_name):
     return response.json().get("data", {}).get("results", [])
 
 
-def fetch_product_description(product_id):
+# TODO: Verificar si aun se puede usar este endpoint ya que saga parece
+# que ha cambiado el endpoint de la API
+def fetch_api_product_extra_details(product_id):
     url = PRODUCT_URL.format(product_id)
     response = requests.get(url, timeout=10)
     if response.status_code != 200:
         return None
     return response.json().get("data", {}).get("description")
+
+
+def fetch_html_product_extra_details(product_url):
+    """
+    Se hace un request a la url de un producto para obtener su descripcion
+    y categoria de producto.
+    """
+    response = requests.get(product_url, timeout=10)
+    if response.status_code != 200:
+        return None
+    return response.text
